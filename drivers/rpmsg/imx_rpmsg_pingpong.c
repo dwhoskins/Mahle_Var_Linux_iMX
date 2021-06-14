c0cbc42ab02d5ef975d28492bb3cfc199d2331f6
dwhoskins <dustin.hoskins@mahle.com>
2021-06-14 09:04:38 -0700

    moving source

--- drivers/rpmsg/imx_rpmsg_pingpong.c
+++ drivers/rpmsg/imx_rpmsg_pingpong.c
@@ -10,8 +10,7 @@
 
 #define MSG		"hello world!"
 
-static int rpmsg_pingpong_cb(struct rpmsg_device *rpdev, void *data, int len,
-						void *priv, u32 src)
+static int rpmsg_m4_cb(struct rpmsg_device *rpdev, void *data, int len, void *priv, u32 src)
 {
 	int err;
 	unsigned int rpmsg_pingpong;
@@ -25,37 +24,41 @@ static int rpmsg_pingpong_cb(struct rpmsg_device *rpdev, void *data, int len,
 		dev_info(&rpdev->dev, "goodbye!\n");
 		return 0;
 	}
+	
 	rpmsg_pingpong++;
 	err = rpmsg_sendto(rpdev->ept, (void *)(&rpmsg_pingpong), 4, src);
 
-	if (err)
+	if(err)
+	{
 		dev_err(&rpdev->dev, "rpmsg_send failed: %d\n", err);
+	}
 
 	return err;
 }
 
-static int rpmsg_pingpong_probe(struct rpmsg_device *rpdev)
+static int rpmsg_m4_probe(struct rpmsg_device *rpdev)
 {
 	int err;
 	unsigned int rpmsg_pingpong;
 
-	dev_info(&rpdev->dev, "new channel: 0x%x -> 0x%x!\n",
-			rpdev->src, rpdev->dst);
+	dev_info(&rpdev->dev, "new channel: 0x%x -> 0x%x!\n", rpdev->src, rpdev->dst);
 
 	/*
 	 * send a message to our remote processor, and tell remote
 	 * processor about this channel
 	 */
+	
 	err = rpmsg_send(rpdev->ept, MSG, strlen(MSG));
-	if (err) {
+	if(err)
+	{
 		dev_err(&rpdev->dev, "rpmsg_send failed: %d\n", err);
 		return err;
 	}
 
 	rpmsg_pingpong = 0;
-	err = rpmsg_sendto(rpdev->ept, (void *)(&rpmsg_pingpong),
-			   4, rpdev->dst);
-	if (err) {
+	err = rpmsg_sendto(rpdev->ept, (void *)(&rpmsg_pingpong), 4, rpdev->dst);
+	if(err)
+	{
 		dev_err(&rpdev->dev, "rpmsg_send failed: %d\n", err);
 		return err;
 	}
@@ -63,38 +66,39 @@ static int rpmsg_pingpong_probe(struct rpmsg_device *rpdev)
 	return 0;
 }
 
-static void rpmsg_pingpong_remove(struct rpmsg_device *rpdev)
+static void rpmsg_m4_remove(struct rpmsg_device *rpdev)
 {
-	dev_info(&rpdev->dev, "rpmsg pingpong driver is removed\n");
+	dev_info(&rpdev->dev, "rpmsg m4 driver is removed\n");
 }
 
-static struct rpmsg_device_id rpmsg_driver_pingpong_id_table[] = {
-	{ .name	= "rpmsg-openamp-demo-channel" },
-	{ .name	= "rpmsg-openamp-demo-channel-1" },
+static struct rpmsg_device_id rpmsg_driver_m4_id_table[] = 
+{
+	{ .name	= "M4_VCI" },
 	{ },
 };
 
-static struct rpmsg_driver rpmsg_pingpong_driver = {
+static struct rpmsg_driver rpmsg_m4_driver = 
+{
 	.drv.name	= KBUILD_MODNAME,
 	.drv.owner	= THIS_MODULE,
-	.id_table	= rpmsg_driver_pingpong_id_table,
-	.probe		= rpmsg_pingpong_probe,
-	.callback	= rpmsg_pingpong_cb,
-	.remove		= rpmsg_pingpong_remove,
+	.id_table	= rpmsg_driver_m4_id_table,
+	.probe		= rpmsg_m4_probe,
+	.callback	= rpmsg_m4_cb,
+	.remove		= rpmsg_m4_remove,
 };
 
-static int __init init(void)
+static int __init rpmsg_m4_init(void)
 {
-	return register_rpmsg_driver(&rpmsg_pingpong_driver);
+	return register_rpmsg_driver(&rpmsg_m4_driver);
 }
 
-static void __exit fini(void)
+static void __exit rpmsg_m4_exit(void)
 {
-	unregister_rpmsg_driver(&rpmsg_pingpong_driver);
+	unregister_rpmsg_driver(&rpmsg_m4_driver);
 }
-module_init(init);
-module_exit(fini);
+module_init(rpmsg_m4_init);
+module_exit(rpmsg_m4_exit);
 
-MODULE_AUTHOR("Freescale Semiconductor, Inc.");
-MODULE_DESCRIPTION("iMX virtio remote processor messaging pingpong driver");
+MODULE_AUTHOR("Mahle Service Solutions");
+MODULE_DESCRIPTION("iMX virtio remote processor messaging driver");
 MODULE_LICENSE("GPL v2");
